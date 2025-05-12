8c9a0608345d2b590e63d32440806b72286b5d5b
Clément Bouchet <clebouchet@gmail.com>
2025-05-12 22:50:39 +0200

    Setup Cmake

---
+++ 2DEngine/src/Actor/Actor.cpp
@@ -0,0 +1,6 @@
+#include <2DEngine/Actor/Actor.hpp>
+//tempo
+#include<iostream>
+Actor::Actor(){
+    printf("Constructor Actor\n");
+}
---
+++ 2DEngineDemo/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <2DEngine/Actor/Actor.hpp>
+
+int main(){
+    printf("Hello world \n");
+    Actor myActor;
+    return 0;
+}
