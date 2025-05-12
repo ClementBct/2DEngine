2fa3d2e0372e8a8a906beedb2961c62c6ecd11d4
Clément Bouchet <clebouchet@gmail.com>
2025-05-12 21:28:52 +0200

    update

--- build_linux.sh
+++ build_linux.sh
@@ -6,10 +6,8 @@ GENERATOR="Unix Makefiles"
 
 if [ ! -d "$BUILD_DIR" ]; then
     mkdir "$BUILD_DIR"
-else
 fi
 
 cd "$BUILD_DIR"
 cmake .. -G "$GENERATOR"
-echo "[3/3] Compilation du projet..."
 cmake --build .
